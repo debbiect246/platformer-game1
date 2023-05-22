ba01a880947d65d469f5b51f7ce8d58f8a8289f2
Deborah Thompson <deb246@btinternet.com>
2023-05-22 18:51:16 +0100

    sprite added

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -9,10 +9,11 @@ import main.GamePanel;
 public class MouseInputs implements MouseListener, MouseMotionListener {
 
 	private GamePanel gamePanel;
+
 	public MouseInputs(GamePanel gamePanel) {
-		this.gamePanel= gamePanel;
+		this.gamePanel = gamePanel;
 	}
-	
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
@@ -22,12 +23,10 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		gamePanel.setRectPos(e.getX(), e.getY());
-
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("Mouse clicked!");
 
 	}
 
